01570b47539478c6f85acb011a14d98e531729f9
Mehtab Malik <mehtab.malik@angloamerican.com>
2022-09-27 08:16:18 +0100

    Add unit test project and tests

--- Petroineos.IntraDayReporting.Service/CsvGeneratorService.cs
+++ Petroineos.IntraDayReporting.Service/CsvGeneratorService.cs
@@ -14,12 +14,14 @@ namespace Petroineos.IntraDayReporting.Service
         public string GenerateCsv(List<AggregatedTradePosition> trades)
         {
             var rows = trades.Select(t => new { LocalTime = $"{t.Hour:D2}:{t.Minute:D2}", t.Volume });
-            using var memory = new MemoryStream();
-            using var writer = new StreamWriter(memory);
-            using var csv = new CsvHelper.CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture);
-            csv.WriteRecords((IEnumerable)rows);
-            csv.Flush();
-            return System.Text.Encoding.UTF8.GetString(memory.ToArray());
+            using (var memory = new MemoryStream())
+            using (var writer = new StreamWriter(memory))
+            using (var csv = new CsvHelper.CsvWriter(writer, System.Globalization.CultureInfo.InvariantCulture))
+            {
+                csv.WriteRecords(rows);
+                csv.Flush();
+                return System.Text.Encoding.UTF8.GetString(memory.ToArray());
+            }
         }
     }
 }
